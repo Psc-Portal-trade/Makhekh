650db851673252d8ec6f2712c1889e976abda652
NarDeenSamuel <totaemel69@gmail.com>
2025-02-09 00:17:41 +0200

    transloco service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding, withHashLocation, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideTranslocoConfig } from './core/transloco.provider';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+  provideHttpClient(),provideTranslocoConfig(),
+
+
+
+  provideRouter(
+    routes,
+    withHashLocation(),
+    withViewTransitions(),
+    withComponentInputBinding(),
+    withInMemoryScrolling({ anchorScrolling: 'enabled' }),
+
+  )
+  ]
 };
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslocoPipe, TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink,TranslocoPipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  _translocoService = inject(TranslocoService);
+  changeLang(): void {
+    const htmlTag = document.documentElement;
+    let lang = localStorage.getItem('lang');
+      if (lang === 'ar') {
+        htmlTag.setAttribute('dir', 'ltr');
+        htmlTag.setAttribute('lang', 'en');
+        this._translocoService.setActiveLang('en');
+        localStorage.setItem('lang', 'en');
+        lang='en'
+    } else {
+      htmlTag.setAttribute('dir', 'rtl');
+
+      htmlTag.setAttribute('lang', 'ar');
+      this._translocoService.setActiveLang('ar');
+      localStorage.setItem('lang', 'ar');
+      lang='ar'
+
+    }
+    console.log("active lang",lang)
+
+    //update setting with new lang
+    //localStorage.setItem("Language",{la})
+  }
 
 }
